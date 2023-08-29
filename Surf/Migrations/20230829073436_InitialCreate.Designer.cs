c9ae5ef890f2da98da531288d9e04d8fd700fa95
casperpersson <persson1201@gmail.com>
2023-08-29 09:46:36 +0200

    validering med en *

--- Surf/Migrations/20230829073436_InitialCreate.Designer.cs
+++ Surf/Migrations/20230829073436_InitialCreate.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Surf.Data;
 
@@ -10,9 +11,11 @@ using Surf.Data;
 namespace Surf.Migrations
 {
     [DbContext(typeof(SurfContext))]
-    partial class SurfContextModelSnapshot : ModelSnapshot
+    [Migration("20230829073436_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Surf/Models/Surfboard.cs
+++ Surf/Models/Surfboard.cs
@@ -1,17 +1,29 @@
 ﻿using Microsoft.AspNetCore.Routing.Constraints;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Surf.Models
 {
     public class Surfboard
     {
         public int ID { get; set; }
+        
+        [Required(ErrorMessage = "*")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "*")]
         public double Length { get; set; }
+        [Required(ErrorMessage = "*")]
         public double  Width { get; set; }
+        [Required(ErrorMessage = "*")]
         public double Thickness { get; set; }
+        [Required(ErrorMessage = "*")]
         public double Volume { get; set; }
+        [Required(ErrorMessage = "*")]
         public decimal Price { get; set; }
+        [Required(ErrorMessage = "*")]
         public string Type { get; set; }
+        [Required(ErrorMessage = "*")]
         public string Equipment { get; set; }
 
     }
