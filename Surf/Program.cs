269d547f5960665e6f580a7d376d7804aae85443
Nicolaj <nicolaj215@gmail.com>
2023-09-07 21:09:45 +0200

    .

--- Surf/Program.cs
+++ Surf/Program.cs
@@ -2,7 +2,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Surf.Data;
 using Microsoft.AspNetCore.Identity;
-using Surf.Areas.Identity.Data;
 
 namespace Surf
 {
