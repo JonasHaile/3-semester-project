da255a0616e6984ed192b1deea9e9c80dab614a5
Jonas Haile <jonashaile51@gmail.com>
2023-09-08 15:01:38 +0200

    UserController tilføjet

---
+++ Surf/Controllers/UserController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Surf.Data;
+using Surf.Models;
+
+namespace Surf.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly SurfDbContext _context;
+
+        public UserController(SurfDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: User
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Surfboard.ToListAsync());
+        }
+
+        // GET: User/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Surfboard == null)
+            {
+                return NotFound();
+            }
+
+            var surfboard = await _context.Surfboard
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (surfboard == null)
+            {
+                return NotFound();
+            }
+
+            return View(surfboard);
+        }
+
+        // GET: User/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name,Length,Width,Thickness,Volume,Price,Type,Equipment,Image,RowVersion")] Surfboard surfboard)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(surfboard);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(surfboard);
+        }
+
+        // GET: User/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Surfboard == null)
+            {
+                return NotFound();
+            }
+
+            var surfboard = await _context.Surfboard.FindAsync(id);
+            if (surfboard == null)
+            {
+                return NotFound();
+            }
+            return View(surfboard);
+        }
+
+        // POST: User/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,Length,Width,Thickness,Volume,Price,Type,Equipment,Image,RowVersion")] Surfboard surfboard)
+        {
+            if (id != surfboard.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(surfboard);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SurfboardExists(surfboard.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(surfboard);
+        }
+
+        // GET: User/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Surfboard == null)
+            {
+                return NotFound();
+            }
+
+            var surfboard = await _context.Surfboard
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (surfboard == null)
+            {
+                return NotFound();
+            }
+
+            return View(surfboard);
+        }
+
+        // POST: User/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Surfboard == null)
+            {
+                return Problem("Entity set 'SurfDbContext.Surfboard'  is null.");
+            }
+            var surfboard = await _context.Surfboard.FindAsync(id);
+            if (surfboard != null)
+            {
+                _context.Surfboard.Remove(surfboard);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SurfboardExists(int id)
+        {
+          return _context.Surfboard.Any(e => e.ID == id);
+        }
+    }
+}
